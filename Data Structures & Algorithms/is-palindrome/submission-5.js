9293d623bc13b13defa9f52efbd6bd72b2014a78
Bharath Reddy P <118063993+Bharathreddy374@users.noreply.github.com>
2026-08-21 01:02:26 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const cleaned = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+        let i =0;
+        let j = cleaned.length - 1;
+        // while(i<j){
+        //     if(cleaned.charCodeAt(i) !== cleaned.charCodeAt(j)){
+        //         return false
+        //     }
+        //     i++;
+        //     j--;
+
+        // }
+        // return true
+        return cleaned === cleaned.split('').reverse().join('')
+    }
+}
