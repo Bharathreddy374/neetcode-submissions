d82bd621f3f7845858bc4aa164a1ce3e09345d2d
Bharath Reddy P <118063993+Bharathreddy374@users.noreply.github.com>
2026-08-20 14:04:53 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const pre = new Map();
+        for (let i =0 ; i<nums.length;i++){
+            const diff = target - nums[i];
+            if(pre.has(diff)){
+                return [pre.get(diff),i];
+            }
+            pre.set(nums[i],i);
+        }
+        return [];
+    }
+}
